37cccd9d222d22c0a2cc8a384524ac15e4eeaf99
nickg <nickg@signalsciences.com>
2017-05-09 10:31:09 -0700

    improve hugo downloader

--- Makefile
+++ Makefile
@@ -1,18 +1,23 @@
 
+HUGO=./bin/hugo
+
 run: hugo
 	find . -name '*.md' | grep -v 2016-08-21 | xargs misspell
-	./hugo -t client9 -v -D -E -F --watch server --bind 0.0.0.0
+	${HUGO} -t client9 -v -D -E -F --watch server --bind 0.0.0.0
 
 compile: hugo
 	find . -name '*.md' | grep -v 2016-08-21 | xargs misspell
-	./hugo -t client9
+	${HUGO} -t client9
 	minify --html-keep-whitespace --html-keep-end-tags --html-keep-document-tags -r -o public public
-	./hugo -t client9 -v -D -E -F -d public-draft --baseURL http://www-draft.client9.com
+	${HUGO} -t client9 -v -D -E -F -d public-draft --baseURL http://www-draft.client9.com
 	minify --html-keep-whitespace --html-keep-end-tags --html-keep-document-tags -r -o public-draft public-draft
 
 clean:
 	rm -rf public public-draft
+	rm -rf bin
 	git gc --aggressive
 
 hugo:
-	./download-hugo.sh	
+	./download-hugo.sh 0.20.7
+.PHONY: hugo
+
--- download-hugo.sh
+++ download-hugo.sh
@@ -13,10 +13,12 @@ set -e
 # Why:
 #
 # 1. go get is slow
-# "go get ...hugo..." can take up to 30 seconds on travis.ci.
-# but downloading directly rom GitHub releases takes 0.3 seconds.
 #
-# 2. make sure your version is the same as travis.ci
+# "go get ...hugo..." can take up to 30 seconds on travis.ci.
+# but downloading directly from GitHub releases takes 0.3 seconds.
+#
+# 2. making sure the local version is the same as travis.ci
+#
 # Best practice is to make sure your local development version of Hugo
 # is the same as what your CI/CD system (e.g. travis, circle-ci, etc)
 # is using.  This makes sure everyone is using the exact same version.
@@ -25,21 +27,36 @@ set -e
 # https://github.com/spf13/hugo/releases/download/v0.20.7/hugo_0.20.7_Linux-64bit.tar.gz
 #
 
-# this is the default value
-HUGO_LATEST=0.20.7
-
-# or set the version on the command line 
+# Set the version on the command line 
 # as the first arg.
 #
 # ./download-hugo.sh 0.20.0
 #
-VERSION=${1:-$HUGO_LATEST}
+VERSION=$1
+if [ -z "${VERSION}" ]; then
+  echo ""
+  echo "Hugo binary downloader"
+  echo ""
+  echo "Usage:"
+  echo "   $0: _version_"
+  echo "   where _version_ is from https://github.com/spf13/hugo/releases"
+  echo ""
+  exit 1
+fi
+
+# strip leading "v" if any.
+# tarball doesn't use "v", but URL does.
+VERSION=${VERSION#v}
+
+BINDIR="./bin"
+mkdir -p ${BINDIR}
+HUGO=${BINDIR}/hugo
 
 # Do we need to get hugo?
 #
 #
-if [ -f ./hugo ]; then
-  if ./hugo version | grep -q ${VERSION}; then
+if [ -f ${HUGO} ]; then
+  if ${HUGO} version | grep -q ${VERSION}; then
      echo "Hugo ${VERSION} installed"
      exit 0
   fi
@@ -80,6 +97,6 @@ URL=https://github.com/${REPO}/releases/download/v${VERSION}/${TARBALL}
 echo "Downloading ${TARBALL}"
 curl -sSL -o ${TMPDIR}/${TARBALL} ${URL}
 tar -C ${TMPDIR} -xzf ${TMPDIR}/${TARBALL}
-cp ${TMPDIR}/hugo .
-./hugo version
+cp ${TMPDIR}/hugo ${HUGO}
+${HUGO} version
 
