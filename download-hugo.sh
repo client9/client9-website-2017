52d4616b7e20d0cfa7f1c6ba890717bb83dd47b0
nickg <nickg@signalsciences.com>
2017-05-09 09:35:12 -0700

    downloader hugo

--- download-hugo.sh
+++ download-hugo.sh
@@ -1,25 +1,85 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
+# Hugo binary downloader.
+#
+# Downloads the appropriate binary for macOS or linux
+# to speed up your CI buids and to make sure your local Hugo
+# is the same as the Hugo your CI/CD uses.
+#
+# Assumes curl is installed and `tar` supports the `-C` flag
+# (change directory before executing)
+#
+# Why:
+#
+# 1. go get is slow
+# "go get ...hugo..." can take up to 30 seconds on travis.ci.
+# but downloading directly rom GitHub releases takes 0.3 seconds.
+#
+# 2. make sure your version is the same as travis.ci
+# Best practice is to make sure your local development version of Hugo
+# is the same as what your CI/CD system (e.g. travis, circle-ci, etc)
+# is using.  This makes sure everyone is using the exact same version.
+#
+# FYI: URLs on GitHub look like this:
 # https://github.com/spf13/hugo/releases/download/v0.20.7/hugo_0.20.7_Linux-64bit.tar.gz
+#
 
+# this is the default value
+HUGO_LATEST=0.20.7
 
-VERSION=0.20.7
+# or set the version on the command line 
+# as the first arg.
+#
+# ./download-hugo.sh 0.20.0
+#
+VERSION=${1:-$HUGO_LATEST}
 
+# Do we need to get hugo?
+#
+#
+if [ -f ./hugo ]; then
+  if ./hugo version | grep -q ${VERSION}; then
+     echo "Hugo ${VERSION} installed"
+     exit 0
+  fi
+fi
+
+# What OS?
+#
+# Translate what the OS claims it is to what
+# GitHub uses
+#
 HOSTOS=$(uname -s)
 case ${HOSTOS} in
-Darwin) OS=macOS
+Darwin)
+  OS=macOS
   ;;
-Linux)  OS=Linux
+Linux)
+  OS=Linux
   ;;
 *)  echo "unknown platform '${HOSTOS}'"
   exit 1
 esac  
 
-TARBALL=hugo_${VERSION}_${OS}-64bit.tar.gz
-URL=https://github.com/spf13/hugo/releases/download/v${VERSION}/${TARBALL}
-echo "Download ${TARBALL}"
-curl -sSL -o /tmp/${TARBALL} ${URL}
-tar -C /tmp -xzf /tmp/${TARBALL}
-cp /tmp/hugo .
+# Are we 64-bits or 32-bits?
+#
+#
+HOSTCPU=$(uname -m)
+case ${HOSTCPU} in
+x86_64)
+  CPU=64bit
+  ;;
+*) echo "unknown CPU: ${HOSTCPU}"
+  exit 1
+esac
+
+TARBALL=hugo_${VERSION}_${OS}-${CPU}.tar.gz
+REPO=spf13/hugo
+URL=https://github.com/${REPO}/releases/download/v${VERSION}/${TARBALL}
+echo "Downloading ${TARBALL}"
+curl -sSL -o ${TMPDIR}/${TARBALL} ${URL}
+tar -C ${TMPDIR} -xzf ${TMPDIR}/${TARBALL}
+cp ${TMPDIR}/hugo .
 ./hugo version
 
