1cc45cf9400ab311589f1b84da4a914ec8b3264e
nickg <nickg@signalsciences.com>
2017-05-17 03:48:51 -0700

    add new config

---
+++ .s3push.sh
@@ -0,0 +1,20 @@
+base ../client9-website-2017/public-draft
+bucket www-draft.client9.com
+region us-east-1
+
+upload {
+  include *
+  storage-class reduced-redundancy
+  cache-control max-age=3600
+}
+
+upload {
+  include *.html
+  content-language en-US
+  cache-control max-age=3601
+}
+
+upload {
+  include *.gif *.png *.jpg
+  cache-control max-age=86400
+}
