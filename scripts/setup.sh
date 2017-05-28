7b4c0add4c5b32169f36583a93af00ff06b79e08
nickg <nickg@signalsciences.com>
2017-05-28 00:36:43 -0700

    bump misspell

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 ./scripts/godownloader-hugo.sh 0.21
-./scripts/godownloader-misspell.sh 0.1.2
+./scripts/godownloader-misspell.sh 0.2.0
 ./scripts/godownloader-minify.sh
 go get github.com/client9/s3push
