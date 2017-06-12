b20282cba0d513cab720a749ba4e4a9f5e716653
nickg <nickg@signalsciences.com>
2017-06-11 19:46:06 -0700

    try auto-releasing on github pages

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,17 +2,8 @@
 set -ex
 ./scripts/setup.sh
 ./scripts/lint.sh
-case "$TRAVIS_BRANCH" in
-  master)
-    make draft
-    s3push --bucket www-draft.client9.com
-    ;;
-  public)
-    make public
-    s3push --bucket www.client9.com
-    ;;
-  *)
-    echo "Unknown branch $TRAVIS_BRANCH"
-    exit 2
-    ;;
-esac
+make public
+cd public
+git add -A
+git commit -m "update"
+git push origin master
