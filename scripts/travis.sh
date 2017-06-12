0641d3033c7c947d23864601fe06fea8f4d2d97c
nickg <nickg@signalsciences.com>
2017-06-11 20:57:22 -0700

    doco

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ex
+# https://www.client9.com/parallelize-shell-or-bash-scripts-using-xargs/
 cat ./scripts/setup.sh | xargs -P4 -ICMD /bin/sh -exc CMD
 ./scripts/lint.sh
 make public
