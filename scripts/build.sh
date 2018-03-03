fa61fcd128bcff0f09a74f29f63fad6eb652310f
nickg <nickg@signalsciences.com>
2018-03-03 06:02:23 -0800

    travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ex
 ./bin/hugo -t client9
+mkdir -p static/assets
 curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css | csscut -html 'public/**/*.html' > static/assets/bootstrap-csscut.min.css
 ./bin/minify --html-keep-whitespace --html-keep-end-tags --html-keep-document-tags -r -o public public
 
