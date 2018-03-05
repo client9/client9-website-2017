c4b7b266d1de25b3ac1b9f896e48cd5c0b354f1a
nickg <nickg@signalsciences.com>
2018-03-05 14:54:26 -0800

    switch to css cut

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 ./bin/hugo -t client9
 mkdir -p public/assets
-curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css | css cut -html 'public/**/*.html' > public/assets/bootstrap-csscut.min.css
+curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css | css cut --html 'public/**/*.html' > public/assets/bootstrap-csscut.min.css
 ./bin/minify --html-keep-whitespace --html-keep-end-tags --html-keep-document-tags -r -o public public
 
