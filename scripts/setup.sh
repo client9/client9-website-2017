dd8ef1972c64dfd2cf5550c8230eafa6be76f009
nickg <nickg@signalsciences.com>
2018-03-09 09:15:38 -0800

    update installers

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 go get github.com/client9/csstool/css
-./scripts/godownloader-hugo.sh 0.37
-./scripts/godownloader-misspell.sh 0.3.0
+./scripts/godownloader-hugo.sh v0.37.1
+./scripts/godownloader-misspell.sh
 ./scripts/godownloader-minify.sh
 #go get github.com/client9/s3push
