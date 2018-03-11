1924da61ad7fd8f3e2c641d2f91b279eab76f157
nickg <nickg@signalsciences.com>
2018-03-11 11:28:33 -0700

    edits for readbility

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,8 @@
 ./bin/hugo -t client9
 mkdir -p public/assets
 curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css | css cut --html 'public/**/*.html' > public/assets/bootstrap-csscut.min.css
-./bin/minify --html-keep-whitespace --html-keep-end-tags --html-keep-document-tags -r -o public public
-
+./bin/minify \
+    --html-keep-whitespace \
+    --html-keep-end-tags \
+    --html-keep-document-tags \
+    -r -o public public
