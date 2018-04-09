c20131e4f8e82a71421618e413e90b658ffa2b1f
nickg <nickg@signalsciences.com>
2018-04-09 11:33:21 -0700

    bump hugo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 go get github.com/client9/csstool/css
-./scripts/godownloader-hugo.sh v0.38
+./scripts/godownloader-hugo.sh v0.38.2
 ./scripts/godownloader-misspell.sh
 ./scripts/godownloader-minify.sh
 #go get github.com/client9/s3push
