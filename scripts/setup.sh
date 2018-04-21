63f789efde12846f786cf7745f399f3c69d0726c
nickg <nickg@signalsciences.com>
2018-04-20 23:22:29 -0700

    use new installer for csstool

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-go get github.com/client9/csstool/css
-curl -sfL https://install.goreleaser.com/github.com/gohugoio/hugo.sh | bash
-curl -sfL https://install.goreleaser.com/github.com/client9/misspell.sh | bash
-curl -sfL https://install.goreleaser.com/github.com/tdewolff/minify.sh | bash
+curl -sfL https://install.goreleaser.com/github.com/client9/csstool.sh | sh
+curl -sfL https://install.goreleaser.com/github.com/gohugoio/hugo.sh | sh
+curl -sfL https://install.goreleaser.com/github.com/client9/misspell.sh | sh
+curl -sfL https://install.goreleaser.com/github.com/tdewolff/minify.sh | sh
