7fe392252266167f56427afdcb0f92ec3879d108
nickg <nickg@signalsciences.com>
2018-11-07 22:06:29 -0800

    pin minify to 2.3.6

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,4 @@
 curl -sfL https://install.goreleaser.com/github.com/client9/csstool.sh | sh
 curl -sfL https://install.goreleaser.com/github.com/gohugoio/hugo.sh | sh
 curl -sfL https://install.goreleaser.com/github.com/client9/misspell.sh | sh
-curl -sfL https://install.goreleaser.com/github.com/tdewolff/minify.sh | sh
+curl -sfL https://install.goreleaser.com/github.com/tdewolff/minify.sh | sh -s v2.3.6
